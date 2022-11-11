8fefa6979d8e0bab771c8f5174aaa4c4b51c1a8b
viola kaveza <kavezaviola@gmail.com>
2022-11-11 16:18:18 +0300

    made changes to the context.cs

--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace BulkyBookWeb.Data
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
     }
 }
