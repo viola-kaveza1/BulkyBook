5b1f948e22e9ba47c5550eaad09d1508711fa406
viola kaveza <kavezaviola@gmail.com>
2022-11-14 14:09:50 +0300

    tagged the ApplicationDbContext tothe categorycontroller.cs

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -6,11 +6,16 @@ namespace BulkyBookWeb.Controllers
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
-        
-        
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
 
         public IActionResult Index()
         {
+            var objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
