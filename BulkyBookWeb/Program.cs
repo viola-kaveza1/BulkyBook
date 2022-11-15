02353052352571c795a15a85eda6f4b16f0278dd
viola kaveza <kavezaviola@gmail.com>
2022-11-15 14:32:46 +0300

    taggged razor pages on the program.cs

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -9,9 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
             builder.Configuration.GetConnectionString("DefaultConnection")
             ));
-
-
-
+builder.Services.AddRazorPages().WithRazorPagesAtContentRoot();
 var app = builder.Build();
 
         // Configure the HTTP request pipeline.
