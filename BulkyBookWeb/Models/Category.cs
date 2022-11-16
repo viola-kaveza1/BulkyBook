07ed46b05d14885ad39f1891955bc0d54fef71fd
viola kaveza <kavezaviola@gmail.com>
2022-11-16 15:30:35 +0300

    tagged a range in the public string

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -10,6 +10,7 @@ namespace BulkyBookWeb.Models
         [Required]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be bettween 1 and 100 only!!")]
         public int DisplayOder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 
