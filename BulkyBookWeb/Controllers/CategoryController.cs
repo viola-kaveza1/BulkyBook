992964aa757728f87d9f1baac948146bfa9eb8fd
viola kaveza <kavezaviola@gmail.com>
2022-11-16 14:10:34 +0300

    made changes on the contoller category

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -30,9 +30,13 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            if (ModelState.IsValid) 
+            {
             _db.Categories.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
