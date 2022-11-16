f5900ad95989f4bc87a4c0b3ebe9fe826e2afa7b
viola kaveza <kavezaviola@gmail.com>
2022-11-16 11:27:35 +0300

    added a razor view

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -19,5 +19,12 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
+
